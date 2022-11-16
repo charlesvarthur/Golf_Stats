8d3e1d7f3ffb10d54ee11e67e4e8803ca4ab8cd3
Charles Arthur <charles.v.arthur@gmail.com>
2022-11-16 18:36:22 +0000

    updated url...again

--- golf_stats_dash.py
+++ golf_stats_dash.py
@@ -13,7 +13,7 @@ st.set_page_config(page_title="CA Full Golf Stats",
                     layout="wide"
                     )
 
-golf_stats = pd.read_csv("https://raw.githubusercontent.com/charlesvarthur/Golf_Stats/main/full_stats.csv?token=GHSAT0AAAAAAB3I2DXZNYZLT4MKLVMM4JXGY3VFBAA")
+golf_stats = pd.read_csv("https://raw.githubusercontent.com/charlesvarthur/Golf_Stats/main/full_stats.csv?token=GHSAT0AAAAAAB3I2DXYY3VEZQRBFMPVV2UEY3VFW4Q")
     
 st.dataframe(golf_stats)
 

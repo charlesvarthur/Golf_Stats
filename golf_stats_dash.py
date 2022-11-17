2ea5d465ec77f28c751200278e496d32ef3e3aa5
Charles Arthur <charles.v.arthur@gmail.com>
2022-11-17 16:39:54 +0000

    updated sidebar variables

--- golf_stats_dash.py
+++ golf_stats_dash.py
@@ -20,7 +20,7 @@ golf_stats = pd.read_csv("https://raw.githubusercontent.com/charlesvarthur/Golf_
 st.sidebar.header("Data Filters:")
 
 #Filters
-course = st.sidebar.multiselect(
+course_name = st.sidebar.multiselect(
     "Select the course:",
     options=golf_stats["course_name"].unique(),
     default=golf_stats["course_name"].unique()
@@ -39,11 +39,11 @@ par = st.sidebar.multiselect(
 )
 
 #Filter query, referencing filter variables
-# gss = golf_stats.query(
-#     "Course == @course & Round_date == @round_date & Par == @par "
-# )
+gss = golf_stats.query(
+    "course_name == @course_name & round_date == @round_date & par == @par "
+)
 
 #Dataframe to streamlit
-st.dataframe(golf_stats)
+st.dataframe(gss)
 
 #print(golf_stats)

9b0f6f620d1a95499790f9c4937499da2b04cb67
Charles Arthur <charles.v.arthur@gmail.com>
2022-12-15 11:41:33 +0000

    updated filter references

--- golf_stats_dash.py
+++ golf_stats_dash.py
@@ -16,31 +16,34 @@ st.set_page_config(page_title="CA Full Golf Stats",
 #Data source
 golf_stats = pd.read_csv("https://raw.githubusercontent.com/charlesvarthur/Golf_Stats/main/full_stats.csv")
 
+#Total score by course
+score_by_course = golf_stats.loc[:,['course_name','score_vs_par']].groupby(['course_name']).sum()
+
 #Sidebar
 st.sidebar.header("Data Filters:")
 
 #Filters
 course_name = st.sidebar.multiselect(
     "Select the course:",
-    options=golf_stats["course_name"].unique(),
-    default=golf_stats["course_name"].unique()
+    options=score_by_course["course_name"].unique(),
+    default=score_by_course["course_name"].unique()
 )
 
 score_vs_par = st.sidebar.multiselect(
     "Select the score vs the par:",
-    options=golf_stats["score_vs_par"].unique(),
-    default=golf_stats["score_vs_par"].unique()
+    options=score_by_course["score_vs_par"].unique(),
+    default=score_by_course["score_vs_par"].unique()
 )
 
-par = st.sidebar.multiselect(
-    "Select the par:",
-    options=golf_stats["par"].unique(),
-    default=golf_stats["par"].unique()
-)
+# par = st.sidebar.multiselect(
+#     "Select the par:",
+#     options=golf_stats["par"].unique(),
+#     default=golf_stats["par"].unique()
+# )
 
 #Filter query, referencing filter variables
-gss = golf_stats.query(
-    "course_name == @course_name & score_vs_par == @score_vs_par & par == @par "
+gss = score_by_course.query(
+    "course_name == @course_name & score_vs_par == @score_vs_par" #& par == @par "
 )
 
 #Dataframe to streamlit
@@ -50,10 +53,6 @@ st.title(":bar_chart: Golf stats")
 st.markdown("##")
 
 #Create a bar charts with the scores for each round. 
-
-#Total score by course
-score_by_course = golf_stats.loc[:,['course_name','score_vs_par']].groupby(['course_name']).sum()
-
 fig_score_by_course = px.bar(
     score_by_course,
     x=score_by_course.index,

a575ce5c5286b2593ef75ae89df5ca4d0d599ec6
Charles Arthur <charles.v.arthur@gmail.com>
2022-12-22 11:38:14 +0000

    variable changes

--- golf_stats_dash.py
+++ golf_stats_dash.py
@@ -31,11 +31,18 @@ st.bar_chart(score_vs_par_by_course)
 
 course_var = st.selectbox('Select a course to for hole specific averages:',['Alton Golf Club','Ampfield Golf Club','Boundary Lakes','Etchinghill Golf Trust','Godstone Golf Club','Hurtmore Golf Club','Paultons Golf Centre','Southampton Municapal Golf Course','The Oaks'])
 
+if course_var in ['Alton Golf Club','Ampfield Golf Club','Boundary Lakes','Etchinghill Golf Trust','Godstone Golf Club','Hurtmore Golf Club','Paultons Golf Centre','Southampton Municapal Golf Course','The Oaks']:
+    golf_stats_tb = golf_stats[golf_stats['course_name'] == course_var]
+else:
+    pass
+
+
+
 sns.set_theme(style = 'darkgrid', palette='deep')
 sns.axes_style("darkgrid")
 fig, ax = plt.subplots()
 ax = sns.barplot(data = golf_stats.loc[['course_name'] == course_var], x = golf_stats['hole_number'], y = golf_stats['score'].mean())
-plt.title('Characteristics of penguins')#.format(selected_species))
+plt.title('Average Score by course')#.format(selected_species))
 plt.xlabel('Hole Number')
 plt.ylabel('Average Score Per hole')
 st.pyplot(fig)  

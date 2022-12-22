8ef77415dbe0907967d7a6fe24c2663e59358752
Charles Arthur <charles.v.arthur@gmail.com>
2022-12-22 13:45:36 +0000

    typo

--- golf_stats_dash.py
+++ golf_stats_dash.py
@@ -52,5 +52,5 @@ else:
 
 st.subheader('Average Hole Score for '+ course_var)
 fig2 = alt.Chart(avg_hole_score_tb).mark_bar(size=20).encode(x = 'hole_number', y = 'score', 
-color=alt.Color(scheme='yellowgreenblue').properties(width=700,height=300)
+color=alt.Color(scheme='yellowgreenblue')).properties(width=700,height=300)
 st.altair_chart(fig2)

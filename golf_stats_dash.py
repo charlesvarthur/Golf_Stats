85fe79f7c021ffca243c1f59242f01b11de838e5
Charles Arthur <charles.v.arthur@gmail.com>
2022-12-22 13:01:15 +0000

    import numpy and floor score

--- golf_stats_dash.py
+++ golf_stats_dash.py
@@ -7,6 +7,7 @@ import plotly_express as px
 import streamlit as st
 import matplotlib.pyplot as plt
 import seaborn as sns
+import numpy as np
 
 #Steamlit app basic config
 st.set_page_config(page_title="Full Golf Stats",
@@ -33,7 +34,7 @@ course_var = st.selectbox('Select a course to for hole specific averages:',['Alt
 
 if course_var in ['Alton Golf Club','Ampfield Golf Club','Boundary Lakes','Etchinghill Golf Trust','Godstone Golf Club','Hurtmore Golf Club','Paultons Golf Centre','Southampton Municapal Golf Course','The Oaks']:
     golf_stats_tb = pd.DataFrame(golf_stats.loc[golf_stats['course_name'] == course_var])
-    golf_stats_tb = pd.DataFrame(golf_stats_tb.loc[:,['course_name','hole_number','score']].groupby(['course_name','hole_number'], as_index=False).mean().astype(float).round(2))
+    golf_stats_tb = pd.DataFrame(golf_stats_tb.loc[:,['course_name','hole_number','score']].groupby(['course_name','hole_number'], as_index=False).mean().apply(np.floor))
     st.write(golf_stats_tb)
 else:
     pass

4ff92fde339615031885d7d7a67191244fa8356f
Charles Arthur <charles.v.arthur@gmail.com>
2022-12-22 12:06:16 +0000

    comment out dataset write

--- golf_stats_dash.py
+++ golf_stats_dash.py
@@ -33,7 +33,7 @@ course_var = st.selectbox('Select a course to for hole specific averages:',['Alt
 
 if course_var in ['Alton Golf Club','Ampfield Golf Club','Boundary Lakes','Etchinghill Golf Trust','Godstone Golf Club','Hurtmore Golf Club','Paultons Golf Centre','Southampton Municapal Golf Course','The Oaks']:
     golf_stats_tb = pd.DataFrame(golf_stats.loc[golf_stats['course_name'] == course_var])
-    st.write(golf_stats_tb)
+    #st.write(golf_stats_tb)
 else:
     pass
 

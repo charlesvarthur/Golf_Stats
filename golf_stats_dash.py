a2e0ddd1be0681af545647c8e8d6cc2d89d15bc9
Charles Arthur <charles.v.arthur@gmail.com>
2022-12-27 16:53:06 +0000

    score n par test

--- golf_stats_dash.py
+++ golf_stats_dash.py
@@ -100,3 +100,6 @@ fig4 = alt.Chart(round_hole_scores).mark_bar(size=20).encode(x = 'hole_number', 
 (fig4).properties(width=alt.Step(30))
 (fig4).encoding.x.title='hole number'
 st.altair_chart(fig4, use_container_width=True)
+
+score_n_par = round_hole_scores['par'].astype(str) + round_hole_scores['score'].astype(str)
+st.write(score_n_par)

a8f684688abf4ed10df3bb18b521036b5e2ee820
Charles Arthur <charles.v.arthur@gmail.com>
2022-12-27 15:13:10 +0000

    test round_hole_scores

--- golf_stats_dash.py
+++ golf_stats_dash.py
@@ -92,10 +92,11 @@ round_dates = round_dates['round_date'].values.tolist()
 
 datebox=st.selectbox('Which date would you like scores from?', round_dates[:])
 
-
-
 #Fix this next. 
 round_hole_scores = pd.DataFrame(golf_stats.loc[golf_stats['course_name'] == course_var & golf_stats['round_date'] == datebox])
+st.write(round_hole_scores)
+
+
 st.subheader('Scores by Round Date for ' + course_var)
 
 fig4 = alt.Chart(round_hole_scores).mark_bar(size=20).encode(x = 'hole_number', y = 'score',

d5d0926090a6218d6f92829ced1de5a10b0dc0f3
Charles Arthur <charles.v.arthur@gmail.com>
2022-12-27 15:50:50 +0000

    combined lines

--- golf_stats_dash.py
+++ golf_stats_dash.py
@@ -86,13 +86,11 @@ fig3.encoding.y.title='total score'
 st.altair_chart(fig3, use_container_width=True)
 
 #Score & par
-round_dates = pd.DataFrame(golf_stats.loc[golf_stats['course_name'] == course_var, ['round_date']]).drop_duplicates()
-round_dates = pd.DataFrame(round_dates).reset_index(drop=True)
+round_dates = pd.DataFrame(golf_stats.loc[golf_stats['course_name'] == course_var, ['round_date']]).drop_duplicates().reset_index(drop=True)
 round_dates = round_dates['round_date'].values.tolist()
 
 datebox=st.selectbox('Which date would you like scores from?', round_dates[:])
 
-#Fix this next. 
 round_hole_scores = pd.DataFrame(golf_stats.loc[(golf_stats['course_name'] == course_var) & (golf_stats['round_date'] == datebox)])
 st.subheader('Scores by Round Date for ' + course_var)
 

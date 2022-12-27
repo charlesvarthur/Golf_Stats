fce8a0bc2f0b0e760d54cbf453a8763d574ff78c
Charles Arthur <charles.v.arthur@gmail.com>
2022-12-27 14:07:39 +0000

    test stop

--- golf_stats_dash.py
+++ golf_stats_dash.py
@@ -85,8 +85,10 @@ fig3.encoding.x.title='round_date'
 fig3.encoding.y.title='total score'
 st.altair_chart(fig3, use_container_width=True)
 
+st.stop()
+
 #Score & par
-round_dates = pd.DataFrame(golf_stats.loc[:,['course_name'] == course_var, ['course_name','round_date']])
+round_dates = pd.DataFrame(golf_stats.loc[:,['course_name'] == course_var, ['round_date']])
 st.write(round_dates)
 round_dates = pd.DataFrame(round_dates).reset_index(drop=True)
 round_dates = round_dates['round_date'].values.tolist()

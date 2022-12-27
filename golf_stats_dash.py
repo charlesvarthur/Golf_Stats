2abe118f4b9b4186da70235a4eb8c91fc98377b8
Charles Arthur <charles.v.arthur@gmail.com>
2022-12-27 14:03:07 +0000

    test write first round_dates query

--- golf_stats_dash.py
+++ golf_stats_dash.py
@@ -87,6 +87,7 @@ st.altair_chart(fig3, use_container_width=True)
 
 #Score & par
 round_dates = pd.DataFrame(golf_stats.loc[:,['course_name'] == course_var, ['round_date']])
+st.write(round_dates)
 round_dates = pd.DataFrame(round_dates).reset_index(drop=True)
 round_dates = round_dates['round_date'].values.tolist()
 

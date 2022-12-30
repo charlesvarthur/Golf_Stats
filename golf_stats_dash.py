666a808ae1c8be4dd6b2c3c80bb8b68ea6b778d1
Charles Arthur <charles.v.arthur@gmail.com>
2022-12-30 22:14:55 +0000

    test run of pg connection

--- golf_stats_dash.py
+++ golf_stats_dash.py
@@ -9,13 +9,27 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
 import altair as alt
-# import psycopg2 as pg
+import psycopg2 as pg
 
-# @st.experimental_singleton
-# def init_connection():
-#     return pg.connect(**st.secrets["postgres"])
+@st.experimental_singleton
+def init_connection():
+    return pg.connect(**st.secrets["postgres"])
 
-# conn = init_connection()
+conn = init_connection()
+
+# Perform query.
+# Uses st.experimental_memo to only rerun when the query changes or after 10 min.
+@st.experimental_memo(ttl=600)
+def run_query(query):
+    with conn.cursor() as cur:
+        cur.execute(query)
+        return cur.fetchall()
+
+rows = run_query("SELECT * from scores;")
+
+# Print results.
+for row in rows:
+    st.write(f"{row[0]} has a :{row[1]}:")
 
 #Steamlit app basic config
 st.set_page_config(page_title="Full Golf Stats",

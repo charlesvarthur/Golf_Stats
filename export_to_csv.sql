5bd8cf1012e1c41b9e590203a55cf5e39a51fb0a
Charles Arthur <charles.v.arthur@gmail.com>
2023-03-27 16:03:44 +0100

    commit with datetime

--- export_to_csv.sql
+++ export_to_csv.sql
@@ -1,1 +1,12 @@
-COPY (select * from full_stats) TO '/Users/charlesarthur/Documents/Data_Projects/Golf_Stats/full_stats.csv' DELIMITER ',' CSV HEADER;
+DO
+$$DECLARE
+	datestr text := to_char(current_timestamp, 'YYYY-MM-DD-HH24_MI_SS');
+
+BEGIN
+	EXECUTE	format(
+		'COPY ' 
+		'(select * from full_stats) '
+		'TO %L CSV HEADER',
+		'/Users/charlesarthur/Documents/Data_Projects/Golf_Stats/full_stats_' || datestr || '.csv'
+		);
+END;$$;

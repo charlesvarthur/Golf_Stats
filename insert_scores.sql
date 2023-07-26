cddc40c79c82643efa1bf031c8b548ebdfbae653
Charles Arthur <charles.v.arthur@gmail.com>
2023-07-26 14:28:08 +0100

    added round data for 21-08

--- insert_scores.sql
+++ insert_scores.sql
@@ -1,23 +1,23 @@
 --inserting scores 
 
--- insert into scores VALUES (1,8,127,10,5);
--- insert into scores VALUES (1,8,128,10,8);
--- insert into scores VALUES (1,8,129,10,5);
--- insert into scores VALUES (1,8,130,10,5);
--- insert into scores VALUES (1,8,131,10,6);
--- insert into scores VALUES (1,8,132,10,4);
--- insert into scores VALUES (1,8,133,10,8);
--- insert into scores VALUES (1,8,134,10,5);
--- insert into scores VALUES (1,8,135,10,6);
--- insert into scores VALUES (1,8,136,10,6);
--- insert into scores VALUES (1,8,137,10,6);
--- insert into scores VALUES (1,8,138,10,5);
--- insert into scores VALUES (1,8,139,10,7);
--- insert into scores VALUES (1,8,140,10,7);
--- insert into scores VALUES (1,8,141,10,5);
--- insert into scores VALUES (1,8,142,10,8);
--- insert into scores VALUES (1,8,143,10,7);
--- insert into scores VALUES (1,8,144,10,7);
+-- insert into scores VALUES (1,8,109,38,5);
+-- insert into scores VALUES (1,8,110,38,8);
+-- insert into scores VALUES (1,8,111,38,9);
+-- insert into scores VALUES (1,8,112,38,4);
+-- insert into scores VALUES (1,8,113,38,4);
+-- insert into scores VALUES (1,8,114,38,7);
+-- insert into scores VALUES (1,8,115,38,4);
+-- insert into scores VALUES (1,8,116,38,6);
+-- insert into scores VALUES (1,8,117,38,6);
+-- insert into scores VALUES (1,8,118,38,5);
+-- insert into scores VALUES (1,8,119,38,6);
+-- insert into scores VALUES (1,8,120,38,9);
+-- insert into scores VALUES (1,8,121,38,4);
+-- insert into scores VALUES (1,8,122,38,6);
+-- insert into scores VALUES (1,8,123,38,5);
+-- insert into scores VALUES (1,8,124,38,6);
+-- insert into scores VALUES (1,8,125,38,7);
+-- insert into scores VALUES (1,8,126,38,7);
 
 -- insert into scores VALUES (1,13,55,36,4);
 -- insert into scores VALUES (1,13,56,36,5);
@@ -37,9 +37,9 @@
 -- ON hs.course_id = c.course_id
 -- where c.course_id=6
 -- order by 1;
---select hole_number,hole_id,notes from hole_stats where course_id=8 order by 2;
+--select hole_number,hole_id,notes from hole_stats where course_id=7 order by 2;
 --select * from course;
-select * from scores where round_id=10 order by 3;
+select * from scores where round_id=38 order by 3;
 --select sum(score) from scores where round_id=37 and player_id=1;
 -- select * from player
 --select * from round

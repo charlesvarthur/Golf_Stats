dc5d92b4d389af570cab19a301efe3cef077f6f9
Charles Arthur <charles.v.arthur@gmail.com>
2024-04-01 15:24:03 +0100

    updated round 44

--- insert_scores.sql
+++ insert_scores.sql
@@ -1,25 +1,25 @@
 --inserting scores 
 
--- insert into scores VALUES (1,20,307,43,6);
--- insert into scores VALUES (1,20,308,43,4);
--- insert into scores VALUES (1,20,309,43,7);
--- insert into scores VALUES (1,20,310,43,8);
--- insert into scores VALUES (1,20,311,43,5);
--- insert into scores VALUES (1,20,312,43,5);
--- insert into scores VALUES (1,20,313,43,6);
--- insert into scores VALUES (1,20,314,43,6);
--- insert into scores VALUES (1,20,315,43,6);
--- insert into scores VALUES (1,20,316,43,9);
--- insert into scores VALUES (1,20,317,43,8);
--- insert into scores VALUES (1,20,318,43,5);
--- insert into scores VALUES (1,20,319,43,8);
--- insert into scores VALUES (1,20,320,43,9);
--- insert into scores VALUES (1,20,321,43,6);
--- insert into scores VALUES (1,20,322,43,5);
--- insert into scores VALUES (1,20,323,43,5);
--- insert into scores VALUES (1,20,324,43,9);
+-- insert into scores VALUES (1,20,1,44,5);
+-- insert into scores VALUES (1,20,2,44,5);
+-- insert into scores VALUES (1,20,3,44,7);
+-- insert into scores VALUES (1,20,4,44,3);
+-- insert into scores VALUES (1,20,5,44,7);
+-- insert into scores VALUES (1,20,6,44,5);
+-- insert into scores VALUES (1,20,7,44,7);
+-- insert into scores VALUES (1,20,8,44,5);
+-- insert into scores VALUES (1,20,9,44,7);
+-- insert into scores VALUES (1,20,10,44,4);
+-- insert into scores VALUES (1,20,11,44,4);
+-- insert into scores VALUES (1,20,12,44,6);
+-- insert into scores VALUES (1,20,13,44,4);
+-- insert into scores VALUES (1,20,14,44,7);
+-- insert into scores VALUES (1,20,15,44,6);
+-- insert into scores VALUES (1,20,16,44,7);
+-- insert into scores VALUES (1,20,17,44,4);
+-- insert into scores VALUES (1,20,18,44,6);
 
--- alter table scores drop column course_id;
+
 
 -- insert into scores VALUES (1,13,55,36,4);
 -- insert into scores VALUES (1,13,56,36,5);
@@ -39,9 +39,9 @@
 -- ON hs.course_id = c.course_id
 -- where c.course_id=6
 -- order by 1;
---select hole_number,hole_id,notes from hole_stats where course_id=20 order by 2;
+--select hole_number,hole_id,notes from hole_stats where course_id=1 order by 2;
 --select * from course;
-select * from scores where round_id=43 order by 3;
---select sum(score) from scores where round_id=43 and player_id=1;
+select * from scores where round_id=44 order by 3;
+--select sum(score) from scores where round_id=44 and player_id=1;
 -- select * from player
 --select * from round

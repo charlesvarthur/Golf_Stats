574bf84568a41558a6fb327b4773ed0e70fa0069
Charles Arthur <charles.v.arthur@gmail.com>
2024-05-01 19:35:56 +0100

    updated tournament scores

--- insert_scores.sql
+++ insert_scores.sql
@@ -1,23 +1,24 @@
 --inserting scores 
+--SELECT * FROM PLAYER
 
--- insert into scores VALUES (1,4,64,46,6);
+-- insert into scores VALUES (1,4,64,46,7);
 -- insert into scores VALUES (1,4,65,46,3);
 -- insert into scores VALUES (1,4,66,46,9);
--- insert into scores VALUES (1,4,67,46,5);
--- insert into scores VALUES (1,4,68,46,4);
--- insert into scores VALUES (1,4,69,46,7);
+-- insert into scores VALUES (1,4,67,46,4);
+-- insert into scores VALUES (1,4,68,46,9);
+-- insert into scores VALUES (1,4,69,46,9);
 -- insert into scores VALUES (1,4,70,46,5);
 -- insert into scores VALUES (1,4,71,46,5);
--- insert into scores VALUES (1,4,72,46,5);
--- insert into scores VALUES (1,4,73,46,4);
--- insert into scores VALUES (1,4,74,46,5);
+-- insert into scores VALUES (1,4,72,46,9);
+-- insert into scores VALUES (1,4,73,46,9);
+-- insert into scores VALUES (1,4,74,46,6);
 -- insert into scores VALUES (1,4,75,46,7);
--- insert into scores VALUES (1,4,76,46,5);
+-- insert into scores VALUES (1,4,76,46,4);
 -- insert into scores VALUES (1,4,77,46,6);
--- insert into scores VALUES (1,4,78,46,9);
--- insert into scores VALUES (1,4,79,46,3);
--- insert into scores VALUES (1,4,80,46,4);
--- insert into scores VALUES (1,4,81,46,5);
+-- insert into scores VALUES (1,4,78,46,7);
+-- insert into scores VALUES (1,4,79,46,5);
+-- insert into scores VALUES (1,4,80,46,5);
+-- insert into scores VALUES (1,4,81,46,9);
 
 
 
@@ -41,7 +42,7 @@
 -- order by 1;
 --select hole_number,hole_id,notes from hole_stats where course_id=4 order by 2;
 --select * from course;
-select * from scores where round_id=46 order by 3;
---select sum(score) from scores where round_id=46 and player_id=1;
+--select * from scores where round_id=46 order by 3;
+select sum(score) from scores where round_id=46 and player_id=1;
 -- select * from player
 --select * from round
